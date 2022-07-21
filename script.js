135fce58aea39a00761669655c438dbc65c2c2e8
Oleh Zazhyrko <kosovo1989@gmail.com>
2022-07-21 11:33:35 +0200

    add solution with RegExp

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const input = document.querySelector(".search");
 const list = document.querySelectorAll("li");
 
+/*
 const searchEngine = (e) => {
 	const text = e.target.value.toLowerCase();
 
@@ -14,3 +15,19 @@ const searchEngine = (e) => {
 };
 
 input.addEventListener("keyup", searchEngine);
+*/
+
+//solution with RegExp
+const filterList = () => {
+	list.forEach((item) => {
+		const match = new RegExp(input.value, "i").test(item.textContent);
+
+		if (!match) {
+			item.style.display = "none";
+		} else {
+			item.style.display = "block";
+		}
+	});
+};
+
+input.addEventListener("keyup", filterList);
